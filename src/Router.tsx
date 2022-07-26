614e1ab152e955e0294c69e89433440c3ce5ece1
Raph2ll <raphael.ph2ll@gmail.com>
2022-07-26 15:01:56 -0300

    feat(Router): new paths

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+import Employees from './pages/Employees';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/employees" element={<Employees />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
